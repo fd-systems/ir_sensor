757d396747d825a36f903bcb4302db7123dd5f47
ziggoon <79215783+ziggoon@users.noreply.github.com>
2021-08-24 15:45:04 -0500

    machine learning tests
    testing decision making tree
    
    correlation tests using red wine data

---
+++ ml/basic_ml.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot
+from pandas import read_csv
+import numpy
+
+names = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar', 'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol', 'quality']
+data = read_csv(r"~/projects/ir_sensor/ml/winequality.csv", header=None, names=names)
+correlations = data.corr()
+fig = pyplot.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(correlations, vmin=-1, vmax=1)
+fig.colorbar(cax)
+ticks = numpy.arange(0,12,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(names)
+ax.set_yticklabels(names)
+pyplot.show()
